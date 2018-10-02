ed6af5fab298339a7753552e412745472bbee001
pvictor <perrier.victor@gmail.com>
2018-10-02 22:28:03 +0200

    import pipe

---
+++ R/topogRam-package.R
@@ -0,0 +1,24 @@
+#' An \code{htmlwidget} to create Continuous Cartogram in 'D3.js'
+#'
+#' This package allow you to use cartogram-chart.js (\url{https://github.com/vasturiano/cartogram-chart}),
+#' an interactive contiguous cartogram reusable chart for visualizing geographical data.
+#'
+#' @name topogRam-package
+#' @docType package
+#' @author Victor Perrier (@@dreamRs_fr)
+NULL
+
+#' topogRam exported operators and S3 methods
+#'
+#' The following functions are imported and then re-exported
+#' from the topogRam package to avoid listing the magrittr
+#' as Depends of topogRam
+#'
+#' @name topogRam-exports
+NULL
+
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @export
+#' @rdname topogRam-exports
+NULL
