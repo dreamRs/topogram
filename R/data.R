c748cf1201987b8583ada5c34015d249e09c8163
pvictor <perrier.victor@gmail.com>
2021-11-05 18:43:23 +0100

    removed old data + data for france employment

--- R/data.R
+++ R/data.R
@@ -1,64 +1,24 @@
 
-#' French population estimate as at 1er January 2018
+#' @title World Countries
 #'
-#' Data by department, gender and age.
+#' @description World countries with data such as population estimate, GDP,
+#'  CO2 emissions and share of electricity production from renewables.
 #'
-#' @format A data frame with 101 observations on the following 20 variables.
-#' \describe{
-#'   \item{\code{departements_code}}{Department code}
-#'   \item{\code{departements_lib}}{Department label}
-#'   \item{\code{ensemble_0_a_19_ans}}{Total population of people aged 0-19 years}
-#'   \item{\code{ensemble_20_a_39_ans}}{Total population of people aged 20-39 years}
-#'   \item{\code{ensemble_40_a_59_ans}}{Total population of people aged 40-59 years}
-#'   \item{\code{ensemble_60_a_74_ans}}{Total population of people aged 60-74 years}
-#'   \item{\code{ensemble_75_ans_et_plus}}{Total population of people aged 75 and over}
-#'   \item{\code{ensemble_total}}{Total population}
-#'   \item{\code{hommes_0_a_19_ans}}{Total population of men aged 0-19 years}
-#'   \item{\code{hommes_20_a_39_ans}}{Total population of men aged 20-39 years}
-#'   \item{\code{hommes_40_a_59_ans}}{Total population of men aged 40-59 years}
-#'   \item{\code{hommes_60_a_74_ans}}{Total population of men aged 60-74 years}
-#'   \item{\code{hommes_75_ans_et_plus}}{Total population of men aged 75 and over}
-#'   \item{\code{hommes_total}}{Total population of men}
-#'   \item{\code{femmes_0_a_19_ans}}{Total population of women aged 0-19 years}
-#'   \item{\code{femmes_20_a_39_ans}}{Total population of women aged 20-39 years}
-#'   \item{\code{femmes_40_a_59_ans}}{Total population of women aged 40-59 years}
-#'   \item{\code{femmes_60_a_74_ans}}{Total population of women aged 60-74 years}
-#'   \item{\code{femmes_75_ans_et_plus}}{Total population of women aged 75 and over}
-#'   \item{\code{femmes_total}}{Total population of women}
-#' }
+#' @format An 'sf' object containing geometries and data.
 #'
 #' @keywords datasets
 #'
-#' @source INSEE (\url{https://www.insee.fr/fr/statistiques/1893198})
-"pop_france"
+#' @source Natural Earth (via package {rnaturalearth}) for polygons, Our World In Data for CO2 and renewables electricity data.
+"world"
 
 
-
-#' Population of Paris by age in 2014
+#' @title France departments
 #'
-#' An 'sf' object containing geometries and data.
+#' @description France departments with data about structure of total employment by major industry in 2019.
 #'
-#' @format A data frame with 20 observations on the following 15 variables.
-#' \describe{
-#'   \item{\code{CODE_INSEE}}{Code of the district}
-#'   \item{\code{LIB}}{Label of the district}
-#'   \item{\code{NAME}}{Name of the district}
-#'   \item{\code{AGE_00}}{Population under 3 years old}
-#'   \item{\code{AGE_03}}{Population of people aged 3-5 years}
-#'   \item{\code{AGE_06}}{Population of people aged 6-10 years}
-#'   \item{\code{AGE_11}}{Population of people aged 11-17 years}
-#'   \item{\code{AGE_18}}{Population of people aged 18-24 years}
-#'   \item{\code{AGE_25}}{Population of people aged 25-39 years}
-#'   \item{\code{AGE_40}}{Population of people aged 40-54 years}
-#'   \item{\code{AGE_55}}{Population of people aged 55-64 years}
-#'   \item{\code{AGE_65}}{Population of people aged 65-79 years}
-#'   \item{\code{AGE_80}}{Population of people aged 80 and over}
-#'   \item{\code{TOTAL}}{Population total}
-#'   \item{\code{geometry}}{a sfc_POLYGON}
-#' }
+#' @format An 'sf' object containing geometries and data.
 #'
 #' @keywords datasets
 #'
-#' @source INSEE (\url{https://www.insee.fr/fr/statistiques/1893198}) and \url{opendata.paris.fr}
-"paris"
-
+#' @source Natural Earth (via package {rnaturalearth}) for polygons, INSEE for data.
+"france"
